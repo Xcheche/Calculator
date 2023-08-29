a8b65882eb665a2b57e8e14b8dc57b45c2f5da8e
Xcheche <chekwubechukwu23@gmail.com>
2023-08-29 08:30:15 +0100

    in progress

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,16 +8,118 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var num1 = 0, num2 = 0, sum = 0;
+
+  final TextEditingController t1 = TextEditingController(text: '0');
+  final TextEditingController t2 = TextEditingController(text: '0');
+
+  void doAddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 + num2;
+    });
+  }
+
+  void doSub() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 - num2;
+    });
+  }
+
+  void doMult() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 * num2;
+    });
+  }
+
+  void doDiv() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 ~/ num2;
+    });
+  }
+
+  void doClear() {
+    setState(() {
+      t1.text = '0';
+      t2.text = '0';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calculator'),
+        centerTitle: true,
       ),
-      body: Container(
-        child: const Center(
-          child: Text('Calculator App'),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            'Output :$sum',
+            style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              hintText: 'Enter Number 1',
+            ),
+            controller: t1,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              hintText: 'Enter Number 2',
+            ),
+            controller: t2,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 20.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: doAddition,
+                child: const Text('+'),
+              ),
+              ElevatedButton(
+                onPressed: doSub,
+                child: const Text('-'),
+              ),
+              ElevatedButton(
+                onPressed: doMult,
+                child: const Text('*'),
+              ),
+              ElevatedButton(
+                onPressed: doDiv,
+                child: const Text('/'),
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: doClear,
+                    child: const Text('Clear'),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
